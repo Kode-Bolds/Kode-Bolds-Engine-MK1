35eb5a0c3d141d740d3beea93d96effa4c9b0755
JakeScrivener <Jake.Scrivener55@gmail.com>
2019-01-18 22:26:04 +0000

    added light component, created light list in render system which tracks lightcomponents attached to entities and changed shader to draw multiple lights

---
+++ Game_Engine/Components/ComponentLight.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace Game_Engine.Components
+{
+    public class ComponentLight : IComponent
+    {
+        Vector4 position;
+        Vector4 colour;
+
+        public ComponentLight(Vector4 lightPosition, Vector4 lightColour)
+        {
+            position = lightPosition;
+            colour = lightColour;
+        }
+
+        public Vector4 Position
+        {
+            get { return position; }
+        }
+
+        public Vector4 Colour
+        {
+            get { return colour; }
+        }
+
+        public ComponentTypes ComponentType
+        {
+            get { return ComponentTypes.COMPONENT_LIGHT; }
+        }
+    }
+}
